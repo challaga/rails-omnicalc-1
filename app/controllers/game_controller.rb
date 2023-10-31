de06525109636f886e79c12cb37efccc271d0dda
challaga <146873313+challaga@users.noreply.github.com>
2023-10-31 04:01:44 +0000

    half

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,33 @@
+class GameController < ApplicationController
+
+  def new_square 
+    render({ :template => "game_templates/newsquare"})
+  end
+  
+  def square_results
+    @the_num = params.fetch("users_number").to_f
+
+    @the_result = @the_num ** 2
+    render({ :template => "game_templates/squareresults"})
+  end
+
+  def new_square_rt
+    render({ :template => "game_templates/newsquarert"})
+  end
+
+  def squarert_results
+    @the_num = params.fetch("users_number").to_f
+
+    @the_result = Math.sqrt(@the_num)
+    render({ :template => "game_templates/squarertresults"})
+  end
+
+  def new_payment 
+    render({ :template => "game_templates/newpayment"})
+  end
+
+  def payment_results
+
+    render({ :template => "game_templates/paymentresults"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  get("/square/new", { :controller => "game", :action => "new_square"})
+
+  get("/square/results", { :controller => "game", :action => "square_results"})
+
+  get("/square_root/new", { :controller => "game", :action => "new_square_rt"})
+
+  get("/square_root/results", { :controller => "game", :action => "squarert_results"})
+
+  get("/payment/new", { :controller => "game", :action => "new_payment"})
+
+  get("/payment/results", { :controller => "game", :action => "payment_results"})
   
 end
